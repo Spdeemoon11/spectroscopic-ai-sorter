af1e10200f199e0317f35ad22f3c637dfbba0f0a
Spdeemoon11 <vrishank24.yadav@gmail.com>
2026-06-24 18:40:11 +0530

    Add copyright notice

--- run_project.py
+++ run_project.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+# Copyright (c) 2026 Vrishank Yadav
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 """
 Master pipeline script to execute the spectroscopic sorting sequence.
 Ensures correct ordering of the 4 cascade AI models.
